f259d2299df8dfec88d345f865a114f99e9562a0
thanhqhc <chanthanh.quachha@ved.com.vn>
2018-01-17 16:15:34 +0700

    Add code

--- Sources/App/New Group/CloudStrogeManager.swift
+++ Sources/App/New Group/CloudStrogeManager.swift
@@ -1,8 +1,25 @@
 //
-//  StrogeManager.swift
+//  CloudStrogeManager.swift
 //  FileLoggerAPIPackageDescription
 //
 //  Created by Thanh Quach on 1/17/18.
 //
 
 import Foundation
+
+protocol CloudStrogeProtocol {
+
+    func createFolder(path: String)
+    func isExisted(path: String)
+    func createFile(path: String)
+    func getFile(path: String)
+    func searchFile(_ name: String)
+
+    var autoCleanFileInterval: TimeInterval {get set}
+
+
+}
+
+class CloudStrogeManager {
+
+}
